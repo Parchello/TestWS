4862e827bb730599cd1943138ad537b13a76f009
Parchello <parchellos@gmail.com>
2024-09-24 19:22:52 +0300

    webstorm startcd

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is 111 {count}
+          count is 111lkuyfghukgfkuyfukyfgjuhv {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
