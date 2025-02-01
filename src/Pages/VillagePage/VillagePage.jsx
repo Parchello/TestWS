9757ea9ba8cf971510c93785227a5ba6b98b71a4
Parchello <parchellos@gmail.com>
2025-02-01 20:49:13 +0200

    add second village

--- src/Pages/VillagePage/VillagePage.jsx
+++ src/Pages/VillagePage/VillagePage.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 
 import Village1_5 from "../../components/Village/Village1_5";
+import Village2_9 from "../../components/Village/Village2_9";
 
 const VillagePage = () => {
   const { id } = useParams(); // Отримуємо ID села з URL
@@ -10,6 +11,8 @@ const VillagePage = () => {
     switch (id) {
       case "1-5":
         return <Village1_5 />;
+      case "2-9":
+        return <Village2_9 />;
       default:
         return (
           <div>
