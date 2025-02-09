7e7537a9dd21dc788dfa6bbc6405ada08aad542b
Parchello <parchellos@gmail.com>
2025-02-09 13:14:03 +0200

    invenrory list

---
+++ src/data/inventory.js
@@ -0,0 +1,14 @@
+export const inventory = {
+  questItems: [
+    { id: 1, name: "Ancient Scroll", description: "A scroll containing ancient knowledge." },
+    { id: 2, name: "Mystic Key", description: "A key that opens a mysterious door." },
+  ],
+  armory: [
+    { id: 1, name: "Iron Armor", defense: 15, description: "A sturdy set of iron armor." },
+    { id: 2, name: "Leather Boots", defense: 5, description: "Lightweight boots made of leather." },
+  ],
+  weapons: [
+    { id: 1, name: "Sword of Valor", attack: 25, description: "A sword imbued with the power of valor." },
+    { id: 2, name: "Bow of Silence", attack: 15, description: "A bow that shoots arrows silently." },
+  ],
+};
